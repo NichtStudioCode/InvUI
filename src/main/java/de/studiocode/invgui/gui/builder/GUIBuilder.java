73d5b0779d14bb4e6ab3cd11b0e0a6ffa93d54bf
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2021-02-01 11:56:13 +0100

    Improved GUIBuilder

--- src/main/java/de/studiocode/invgui/gui/builder/GUIBuilder.java
+++ src/main/java/de/studiocode/invgui/gui/builder/GUIBuilder.java
@@ -2,16 +2,47 @@ package de.studiocode.invgui.gui.builder;
 
 import de.studiocode.invgui.gui.GUI;
 import de.studiocode.invgui.gui.SlotElement;
+import de.studiocode.invgui.gui.impl.SimpleGUI;
 import de.studiocode.invgui.item.Item;
 import org.bukkit.inventory.ShapedRecipe;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * A utility class to easily construct {@link GUI}s.<br>
+ * A builder class to easily construct {@link GUI}s.<br>
  * It provides similar functionality to Bukkit's {@link ShapedRecipe}, as it
  * allows for a structure String which defines the layout of the {@link GUI}.
  */
-public interface GUIBuilder {
+public class GUIBuilder {
+    
+    protected final int width;
+    protected final int height;
+    private final int size;
+    
+    private final Map<Character, Ingredient> ingredientMap = new HashMap<>();
+    
+    private String structure;
+    
+    public GUIBuilder(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.size = width * height;
+    }
+    
+    /**
+     * Builds the {@link GUI}.
+     *
+     * @return The built {@link GUI}
+     */
+    public GUI build() {
+        SimpleGUI gui = new SimpleGUI(width, height);
+        setSlotElements(gui);
+        return gui;
+    }
     
     /**
      * Sets the structure of the {@link GUI}.
@@ -20,7 +51,13 @@ public interface GUIBuilder {
      *
      * @param structure The structure {@link String}
      */
-    void setStructure(@NotNull String structure);
+    public GUIBuilder setStructure(@NotNull String structure) {
+        String cleanedStructure = structure.replace(" ", "").replace("\n", "");
+        if (cleanedStructure.length() != size) throw new IllegalArgumentException("Structure length does not match size");
+        this.structure = cleanedStructure;
+        
+        return this;
+    }
     
     /**
      * Sets an ingredient for the structure String, which will later be
@@ -29,7 +66,10 @@ public interface GUIBuilder {
      * @param key  The ingredient key
      * @param item The {@link Item}
      */
-    void setIngredient(char key, @NotNull Item item);
+    public GUIBuilder setIngredient(char key, @NotNull Item item) {
+        ingredientMap.put(key, new Ingredient(item));
+        return this;
+    }
     
     /**
      * Sets an ingredient for the structure String, which will later be
@@ -38,13 +78,88 @@ public interface GUIBuilder {
      * @param key         The ingredient key
      * @param slotElement The {@link SlotElement}
      */
-    void setIngredient(char key, @NotNull SlotElement slotElement);
+    public GUIBuilder setIngredient(char key, @NotNull SlotElement slotElement) {
+        ingredientMap.put(key, new Ingredient(slotElement));
+        return this;
+    }
     
-    /**
-     * Builds the {@link GUI}.
-     *
-     * @return The built {@link GUI}
-     */
-    GUI build();
+    protected void setIngredient(char key, int special) {
+        ingredientMap.put(key, new Ingredient(special));
+    }
+    
+    private Ingredient[] parseStructure() {
+        if (structure == null) throw new IllegalStateException("Structure has not been set yet.");
+        
+        Ingredient[] ingredients = new Ingredient[size];
+        int i = 0;
+        for (char c : structure.toCharArray()) {
+            if (c != '.') ingredients[i] = this.ingredientMap.get(c);
+            i++;
+        }
+        
+        return ingredients;
+    }
+    
+    protected void setSlotElements(GUI gui) {
+        Ingredient[] ingredients = parseStructure();
+        for (int i = 0; i < gui.getSize(); i++) {
+            Ingredient ingredient = ingredients[i];
+            if (ingredient != null && ingredient.isSlotElement())
+                gui.setSlotElement(i, ingredient.getSlotElement());
+        }
+    }
+    
+    protected int[] findIndicesOf(int special) {
+        List<Integer> indices = new ArrayList<>();
+        
+        Ingredient[] ingredients = parseStructure();
+        for (int i = 0; i < size; i++) {
+            Ingredient ingredient = ingredients[i];
+            if (ingredient != null && ingredient.isSpecial() && ingredient.getSpecial() == special)
+                indices.add(i);
+        }
+        
+        return indices.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    static class Ingredient {
+        
+        private final SlotElement slotElement;
+        private final int special;
+        
+        public Ingredient(Item item) {
+            this.slotElement = new SlotElement.ItemSlotElement(item);
+            this.special = -1;
+        }
+        
+        public Ingredient(SlotElement slotElement) {
+            this.slotElement = slotElement;
+            this.special = -1;
+        }
+        
+        public Ingredient(int special) {
+            this.special = special;
+            this.slotElement = null;
+        }
+        
+        public SlotElement getSlotElement() {
+            if (isSpecial()) throw new IllegalStateException("Ingredient is special");
+            return slotElement;
+        }
+        
+        public int getSpecial() {
+            if (isSlotElement()) throw new IllegalStateException("Ingredient is item");
+            return special;
+        }
+        
+        public boolean isSlotElement() {
+            return slotElement != null;
+        }
+        
+        public boolean isSpecial() {
+            return slotElement == null;
+        }
+        
+    }
     
 }
