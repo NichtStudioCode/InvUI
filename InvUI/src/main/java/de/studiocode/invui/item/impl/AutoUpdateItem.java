a53d35353566dc3c28e4ddf5c96c5ad54a6fa842
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2022-12-21 13:39:20 +0100

    Fix formatting in AutoUpdateItem

--- InvUI/src/main/java/de/studiocode/invui/item/impl/AutoUpdateItem.java
+++ InvUI/src/main/java/de/studiocode/invui/item/impl/AutoUpdateItem.java
@@ -14,36 +14,35 @@ import java.util.function.Supplier;
  * of ticks.
  */
 public class AutoUpdateItem extends SuppliedItem {
-
+    
     private final int period;
     private BukkitTask task;
-
-
+    
     public AutoUpdateItem(int period, Supplier<? extends ItemProvider> builderSupplier) {
         super(builderSupplier, null);
         this.period = period;
     }
-
+    
     public void start() {
         if (task != null) task.cancel();
         task = Bukkit.getScheduler().runTaskTimer(InvUI.getInstance().getPlugin(), this::notifyWindows, 0, period);
     }
-
+    
     public void cancel() {
         task.cancel();
         task = null;
     }
-
+    
     @Override
     public void addWindow(Window window) {
         super.addWindow(window);
         if (task == null) start();
     }
-
+    
     @Override
     public void removeWindow(Window window) {
         super.removeWindow(window);
         if (getWindows().isEmpty() && task != null) cancel();
     }
-
+    
 }
