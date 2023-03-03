88da7bb141e82efab3e03117cc641edc17f90f8c
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2023-03-03 23:01:12 +0100

    Fix AutoCycle- and AutoUpdateItem

--- invui/src/main/java/xyz/xenondevs/invui/item/impl/AutoCycleItem.java
+++ invui/src/main/java/xyz/xenondevs/invui/item/impl/AutoCycleItem.java
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import xyz.xenondevs.invui.InvUI;
 import xyz.xenondevs.invui.item.Item;
 import xyz.xenondevs.invui.item.ItemProvider;
-import xyz.xenondevs.invui.window.Window;
+import xyz.xenondevs.invui.window.AbstractWindow;
 
 /**
  * An {@link Item} that automatically cycles through a predefined array of
@@ -50,13 +50,13 @@ public class AutoCycleItem extends AbstractItem {
     }
     
     @Override
-    public void addWindow(Window window) {
+    public void addWindow(AbstractWindow window) {
         super.addWindow(window);
         if (task == null) start();
     }
     
     @Override
-    public void removeWindow(Window window) {
+    public void removeWindow(AbstractWindow window) {
         super.removeWindow(window);
         if (getWindows().isEmpty() && task != null) cancel();
     }
