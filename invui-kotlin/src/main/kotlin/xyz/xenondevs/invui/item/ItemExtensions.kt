b94d0254e1a5f7d47ab087226ce4017f6d82a4af
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2025-07-21 18:56:46 +0200

    Remove provider-combining overloads of setItemProvider

--- invui-kotlin/src/main/kotlin/xyz/xenondevs/invui/item/ItemExtensions.kt
+++ invui-kotlin/src/main/kotlin/xyz/xenondevs/invui/item/ItemExtensions.kt
@@ -1,6 +1,25 @@
 package xyz.xenondevs.invui.item
 
+import org.bukkit.inventory.ItemStack
+import xyz.xenondevs.commons.provider.Provider
+import xyz.xenondevs.invui.ExperimentalReactiveApi
+
 /**
  * Calls [Item.notifyWindows] for all items in this [Iterable].
  */
 fun Iterable<Item>.notifyWindows() = forEach { it.notifyWindows() }
+
+@ExperimentalReactiveApi
+fun <S : Item.Builder<*>> S.setItemProvider(
+    provider: Provider<ItemProvider>
+): S {
+    addModifier { item -> provider.observeWeak(item) { weakItem -> weakItem.notifyWindows() } }
+    setItemProvider { provider.get() }
+    return this
+}
+
+@JvmName("setItemProvider1")
+@ExperimentalReactiveApi
+fun <S : Item.Builder<*>> S.setItemProvider(
+    provider: Provider<ItemStack>
+): S = setItemProvider(provider.map(::ItemWrapper))
