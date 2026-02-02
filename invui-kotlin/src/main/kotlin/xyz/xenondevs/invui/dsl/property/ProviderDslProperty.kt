9a9c6a2dacae81f4e4af74505d2bf490dce31610
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-02-02 17:29:12 +0100

    Make ProviderDslProperty non-mutable

--- invui-kotlin/src/main/kotlin/xyz/xenondevs/invui/dsl/property/ProviderDslProperty.kt
+++ invui-kotlin/src/main/kotlin/xyz/xenondevs/invui/dsl/property/ProviderDslProperty.kt
@@ -7,23 +7,21 @@ import xyz.xenondevs.commons.provider.Provider
 import xyz.xenondevs.commons.provider.UnstableProviderApi
 import xyz.xenondevs.commons.provider.immediateFlatten
 import xyz.xenondevs.commons.provider.mutableProvider
-import xyz.xenondevs.invui.NonMutableMutableProvider
+import xyz.xenondevs.commons.provider.provider
 import xyz.xenondevs.invui.dsl.ExperimentalDslApi
 
 @ExperimentalDslApi
 open class ProviderDslProperty<T> internal constructor(
-    private val source: MutableProvider<MutableProvider<T>>,
-    override val identifier: MutableProvider<T> = source.immediateFlatten()
-) : MutableProvider<T> by identifier {
+    private val source: MutableProvider<Provider<T>>,
+    override val identifier: Provider<T> = source.immediateFlatten()
+) : Provider<T> by identifier {
     
-    internal constructor(initial: T) : this(mutableProvider(mutableProvider(initial)))
+    internal constructor(initial: T) : this(mutableProvider(provider(initial)))
     
-    infix fun by(provider: MutableProvider<T>) {
-        source.set(provider)
-    }
+    internal constructor(initial: Provider<T>) : this(mutableProvider(initial))
     
     infix fun by(provider: Provider<T>) {
-        source.set(NonMutableMutableProvider(provider))
+        source.set(provider)
     }
     
     infix fun by(value: T) {
