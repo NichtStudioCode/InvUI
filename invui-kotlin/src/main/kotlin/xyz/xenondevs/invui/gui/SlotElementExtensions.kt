ce41d3d0e79e6ab9284beecf3cb71c0d0bc1bf3e
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-04-18 13:55:52 +0200

    Add default value for InventoryLink background

--- invui-kotlin/src/main/kotlin/xyz/xenondevs/invui/gui/SlotElementExtensions.kt
+++ invui-kotlin/src/main/kotlin/xyz/xenondevs/invui/gui/SlotElementExtensions.kt
@@ -2,6 +2,7 @@ package xyz.xenondevs.invui.gui
 
 import org.bukkit.inventory.ItemStack
 import xyz.xenondevs.commons.provider.Provider
+import xyz.xenondevs.commons.provider.provider
 import xyz.xenondevs.invui.ExperimentalReactiveApi
 import xyz.xenondevs.invui.PropertyAdapter
 import xyz.xenondevs.invui.inventory.Inventory
@@ -15,6 +16,6 @@ import xyz.xenondevs.invui.item.ItemProvider
 fun InventoryLink(
     inventory: Inventory,
     slot: Int,
-    background: Provider<ItemProvider>,
+    background: Provider<ItemProvider> = provider(ItemProvider.EMPTY),
     visualizer: (ItemStack?) -> ItemProvider? = { null }
 ) = SlotElement.InventoryLink(inventory, slot, PropertyAdapter(background), visualizer)
