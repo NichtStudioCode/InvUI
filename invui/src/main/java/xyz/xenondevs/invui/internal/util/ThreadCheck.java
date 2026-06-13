c2b4b5883d96cadd253dac537754d8a82950a5dc
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2026-06-13 14:48:53 +0200

    Throw IllegalState- instead of IllegalArgumentException in ThreadCheck

--- invui/src/main/java/xyz/xenondevs/invui/internal/util/ThreadCheck.java
+++ invui/src/main/java/xyz/xenondevs/invui/internal/util/ThreadCheck.java
@@ -11,7 +11,7 @@ public final class ThreadCheck {
         if (Bukkit.isOwnedByCurrentRegion(entity))
             return;
         
-        throw new IllegalArgumentException(
+        throw new IllegalStateException(
             "A method was called from an incorrect thread. On Paper the correct thread is the server thread. " +
             "On Folia, the correct thread is the thread that owns the viewer (use the viewer's entity scheduler). " +
             "Note that the absence of this exception does not imply correctness. In general and unless otherwise documented, " +
